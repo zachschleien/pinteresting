8290a4c4666ef23511751ee87e546d800d32e03a
zachschleien <zschleien@gmail.com>
2016-05-01 15:05:22 -0400

    Add Bootstrap and customization

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root "pages#home"
-  get "about" => "pages#about"
+  root "pages#home"  # home page localhost:3000
+  get "about" => "pages#about" # about_path
 end
