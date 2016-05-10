66eabbc3de021f42914dfb6a2980827a5f670ba5
zachschleien <zschleien@gmail.com>
2016-05-10 16:19:38 -0400

    Updated Password Requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,4 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  validates :password, 
-            :presence => true,
-            :on => :create,
-            :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
 end
